4b356ffaa7a5c8bdd0db8eeda2db313b75a63637
Mohasin Haque <mohasinhaque23121@gmail.com>
2026-02-26 19:41:02 +0530

    fix: url for backend connectivity

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,12 @@
 export async function queryIssue(userQuery) {
-  const res = await fetch("http://127.0.0.1:8000/query", {
+  //local
+  // const res = await fetch("http://127.0.0.1:8000/query", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify({ user_query: userQuery })
+  // });
+  //prod
+  const res = await fetch("https://ai-agent-project-he7a.onrender.com", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ user_query: userQuery })
